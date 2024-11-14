a5774be8e86aea260e117a07d56e4e6bdcfc18c2
ashwingrras <ashwin@grrassolutions.in>
2024-11-14 18:46:08 +0530

    practice till 14 november 2024

---
+++ src/main/java/org/example/collection_framework/Student.java
@@ -0,0 +1,39 @@
+package org.example.collection_framework;
+
+public class Student
+{
+    int rollNo;
+    String name;
+
+    String address;
+
+    public Student(int rollNo, String name)
+    {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
